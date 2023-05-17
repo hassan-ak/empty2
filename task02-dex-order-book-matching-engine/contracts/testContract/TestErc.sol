12eb7bc71a4c70411ec77f6c9d5a91c7d986de2a
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-05-17 05:53:44 +0500

    Create TestErc.sol

---
+++ task02-dex-order-book-matching-engine/contracts/testContract/TestErc.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "../Orderbook.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestErc is ERC20 {
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) ERC20(name_, symbol_) {}
+
+    function mintToken(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+}
